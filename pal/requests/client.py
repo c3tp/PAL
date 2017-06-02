0d804ec603510343a20dabc982671dbd6627b1a2
Andreas.Abdi <andreasabdi@yahoo.ca>
2017-06-01 17:43:02 -0700

    [C3TP-295] Basic backend calls via boto3 for presigned urls

---
+++ pal/requests/client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import boto3
+import pal.config.defaults as defaults
+import pal.authentication.auth_strategy as auth_strats
+import string
+
+
+def get_client(
+        auth_strategy: auth_strats.AbstractAuthenticationStrategy,
+        username: string,
+        password: string):
+    key_id, key = auth_strategy.authenticate(username, password)
+    print("keys are %s: %s" % (key_id, key))
+    return get_wos_client(
+        key_id=key_id,
+        key=key)
+
+
+def get_wos_client(key_id, key):
+    return get_raw_boto_client(defaults.S3_ENDPOINT, key_id=key_id, key=key)
+
+
+def get_raw_boto_client(s3_endpoint_url, key_id, key):
+    """
+    generates a client based on an endpoint, key_id, key
+    """
+    return boto3.client(
+        service_name="s3",
+        endpoint_url=s3_endpoint_url,
+        aws_access_key_id=key_id,
+        aws_secret_access_key=key,
+        verify=False)
--- pal/requests/request.py
+++ pal/requests/request.py
@@ -1,23 +1,26 @@
 import random
 import string
-
 import boto3
-
 import pal.authentication.auth_strategy as auth_strats
-import pal.config.defaults as defaults
+import pal.requests.client as client
+
+
+def download_file(s3_client: boto3.client, bucket, key):
+    "Using an existing client, return a file from a bucket"
+    # TODO(Andreas)
+    return "stub"
+
+
+def upload_file(s3_client: boto3.client, bucket, key):
+    # TODO(Andreas)
+    return "stub"
 
 
 def generate_request(
         auth_strategy: auth_strats.AbstractAuthenticationStrategy,
         username: string,
         password: string):
-    key_id, key = auth_strategy.authenticate(username, password)
-    print("keys are %s: %s" % (key_id, key))
-    s3_client = __generate_client(
-        s3_endpoint_url=defaults.S3_ENDPOINT,
-        key_id=key_id,
-        key=key)
-    __perform_a_bunch_of_actions(s3_client)
+    __perform_a_bunch_of_actions(client.get_client(auth_strategy, username, password))
 
 
 def __random_string(length=13):
@@ -35,15 +38,6 @@ def __s3_bucket_name(prefix="c3tp-"):
     return prefix + __random_string(random.randint(min_length, max_length)).lower()
 
 
-def __generate_client(s3_endpoint_url, key_id, key):
-    return boto3.client(
-        service_name="s3",
-        endpoint_url=s3_endpoint_url,
-        aws_access_key_id=key_id,
-        aws_secret_access_key=key,
-        verify=False)
-
-
 def __perform_a_bunch_of_actions(s3_client):
     bucket_name = __s3_bucket_name()
     response = s3_client.list_buckets()
@@ -52,14 +46,3 @@ def __perform_a_bunch_of_actions(s3_client):
     print("Bucket List: %s" % buckets)
     s3_client.delete_bucket(Bucket=bucket_name)
 
-
-def test_request():
-    '''Create a bucket and try to list out all buckets, then delete it'''
-    print("Testing requests")
-    s3_client = boto3.client(
-        service_name="s3",
-        endpoint_url=defaults.S3_ENDPOINT,
-        verify=False)
-    response = s3_client.list_buckets()
-    buckets = [bucket['Name'] for bucket in response['Buckets']]
-    print("Bucket List: %s" % buckets)
