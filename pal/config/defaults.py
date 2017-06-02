bce69944220e46c930e46b13f8b2d5cf547d0342
Andreas.Abdi <andreasabdi@yahoo.ca>
2017-06-02 15:29:40 -0700

    Testing uploads/downloads

--- pal/config/defaults.py
+++ pal/config/defaults.py
@@ -4,4 +4,4 @@ Contains the default configurations for things like file locations
 """
 
 CONFIG_FILEPATH = "/etc/pal.conf"
-S3_ENDPOINT = "https://s3-uvic.dev.computecanada.ca"
+S3_ENDPOINT = "http://127.0.0.1:9000"
