c58745e06a69b2eb49c8d45442c7efa43e0d6185
Andreas.Abdi <andreasabdi@yahoo.ca>
2017-06-01 17:56:15 -0700

    [C3TP-293] Started on work to create python library

--- pal/main.py
+++ pal/main.py
@@ -1,6 +1,9 @@
 import sys
 import pal.requests.request as pal_request
 import pal.authentication.dummy_strategy as dummy_strategy
+import pal.requests.presigned as pal_presigned
+import pal.requests.client as pal_client
+import argparse
 
 
 def main(args):
@@ -9,14 +12,37 @@ def main(args):
     Args:
       args ([str]): command line parameter list
     """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', help='pre_upload/pre_download/upload/download')
+    parser.add_argument('bucket')
+    parser.add_argument('key')
+    parser.add_argument('--username', default="lol", help='username help')
+    parser.add_argument('--password', default="wut")
+    parser.add_argument('--dummy-auth', default=True, action="store_true")
+    args = vars(parser.parse_args())
+    print("Hi we tried parsing")
+    if args['dummy_auth'] is False:
+        # TODO(Andreas): We'll do real auth eventually
+        return
+
+    print("we want to do dummy auth")
     strategy = dummy_strategy.DummyAuthenticationStrategy()
-    pal_request.generate_request(strategy, "lol", "wut")
+    client = pal_client.get_client(strategy, args['username'], args['password'])
+
+    result = {
+        'upload': lambda b, k: pal_request.upload_file(client, b, k),
+        'download': lambda b, k: pal_request.download_file(client, b, k),
+        'pre_upload': lambda b, k: pal_presigned.get_presigned_upload(client, b, k),
+        'pre_download': lambda b, k: pal_presigned.get_presigned_download(client, b, k)
+    }[args['command']](args['bucket'], args['key'])
+    return result
 
 
 def run():
     """Entry point for console_scripts
     """
     main(sys.argv[1:])
+
 
 if __name__ == '__main__':
     run()
